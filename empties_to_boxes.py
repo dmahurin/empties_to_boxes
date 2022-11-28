15c039adf8b38e646d8bc8020f013725f6ec86f2
Don Mahurin <@>
2022-11-27 19:08:34 -0800

    process only empties tree assumed from selection

--- empties_to_boxes.py
+++ empties_to_boxes.py
@@ -23,21 +23,25 @@ def replace_with_collection(e):
     collection = bpy.data.collections.new(e.name)
 
     for child in e.children:
-        if child.children is None or len(child.children) == 0:
-            child = replace_with_box(child)
-            #unlink from active collection, relink to our collection
-            bpy.context.collection.objects.unlink(child)
-            collection.objects.link(child)
-        else:
-            child = replace_with_collection(child)
-            collection.children.link(child)
+        if child.type == 'EMPTY':
+            collection_replace_empty(collection, child)
 
     bpy.data.objects.remove(e, do_unlink=True)
 
     return collection
 
-def get_root_node(e):
-	while(e.parent is not None):
+def collection_replace_empty(collection, e):
+    if e.children is None or len(e.children) == 0:
+        e = replace_with_box(e)
+        #unlink from active collection, relink to our collection
+        bpy.context.collection.objects.unlink(e)
+        collection.objects.link(e)
+    else:
+        e = replace_with_collection(e)
+        collection.children.link(e)
+
+def find_ancestor_node(e, pool):
+	while(e.parent is not None and e.parent in pool):
 		e = e.parent
 	return e
 
@@ -48,9 +52,10 @@ class EBOX_OT_create_boxes(bpy.types.Operator):
     bl_options = {'UNDO'}
 
     def execute(self, context):
-        selected_empties_roots = list(set([get_root_node(i) for i in bpy.context.selected_objects if i.type == "EMPTY" and len(i.children) == 0]))
-        for e in selected_empties_roots:
-            e.users_collection[0].children.link(replace_with_collection(e))
+        empties = set([i for i in bpy.context.selected_objects if i.type == "EMPTY"])
+        empties = list(set([find_ancestor_node(i, empties) for i in empties]))
+        for e in empties:
+            collection_replace_empty(e.users_collection[0], e)
 
         return {'FINISHED'}
 
